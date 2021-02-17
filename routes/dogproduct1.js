133e0ac9b5a7deff4ee3d29bfd13cfc62ed82437
eckim817 <elisekim817@gmail.com>
2021-02-16 22:38:08 -0800

    added product info file and search barw

---
+++ routes/dogproduct1.js
@@ -0,0 +1,11 @@
+// By Elise : Created file
+/* If you decide to change the name of this file, 
+we have to change the file name in views, the require()
+in app.js and the path in app.js as well */
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+    res.render('dogproduct1');
+  };
