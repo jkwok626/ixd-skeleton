0da954e6fecea72a09cd0bf6822bcb8b2d7330ea
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-03-04 11:45:57 -0800

    update suggestion

--- routes/add.js
+++ routes/add.js
@@ -1,10 +1,12 @@
 var data = require("../products.json");
 
 exports.addProduct = function(request, response) {   
-    console.log("hi");
 	var productName = request.query.name;
 	var productDescription = request.query.description;
+	var lastId = data.products[data.products.length-1].id;
+	var productId = lastId + 1;
 	var newProduct = {
+		"id": productId,
 		"title": productName,
 		"summary": productDescription,
 		"image": "https://loremflickr.com/400/400"	
