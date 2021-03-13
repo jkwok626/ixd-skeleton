0f1bf760b7ab6817fb3975444ff4941265a6e0c1
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-03-12 19:27:43 -0800

    added stars to reviews

--- routes/addNewReview.js
+++ routes/addNewReview.js
@@ -3,14 +3,19 @@ var data = require("../reviews.json");
 exports.addReview = function (request, response) {
 	// var reviewName = request.query.name;
 	var reviewRating = request.query.rate;
+	console.log(reviewRating);
 	var reviewDescription = request.query.description;
 	var productID = request.query.id;
+	var ratings = "";
+	for(i = 0; i < reviewRating; i++) {
+		ratings+= "★";
+	}
 
 	var newReview = {
 		"product_id": productID,
 		"user_id": 3,
 		"author": "Anonymous Test",
-		"stars": reviewRating,
+		"stars": ratings,
 		"text": reviewDescription
 	};
 	newData = data.reviews.push(newReview);
