d131f09e48a22e22df645b9effce1a6f4b38e0de
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-03-12 19:29:26 -0800

    ffix new reviews stars not showing

--- routes/addNewReview.js
+++ routes/addNewReview.js
@@ -3,19 +3,14 @@ var data = require("../reviews.json");
 exports.addReview = function (request, response) {
 	// var reviewName = request.query.name;
 	var reviewRating = request.query.rate;
-	console.log(reviewRating);
 	var reviewDescription = request.query.description;
 	var productID = request.query.id;
-	var ratings = "";
-	for(i = 0; i < reviewRating; i++) {
-		ratings+= "★";
-	}
 
 	var newReview = {
 		"product_id": productID,
 		"user_id": 3,
 		"author": "Anonymous Test",
-		"stars": ratings,
+		"stars": reviewRating,
 		"text": reviewDescription
 	};
 	newData = data.reviews.push(newReview);
