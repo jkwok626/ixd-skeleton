6a9d5ba845c4725c78e70f23a54b0a12e27b35a4
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-03-13 17:22:45 -0800

    sort products

--- app.js
+++ app.js
@@ -35,13 +35,24 @@ app.engine('handlebars', handlebars({
     },
     times: (n, loop) => {
       var count = '';
-      for (var i  = 0; i < n; ++i) {
+      for (var i = 0; i < n; ++i) {
         count += loop.fn(i);
       }
       return count;
     },
     subtract: (a, b) => {
       return Number(a) - Number(b);
+    },
+    sortProducts: (array, key, reverse) => {
+      return array.sort(function (a, b) {
+        var x = a[key];
+        var y = b[key];
+        if (!reverse) {
+          return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        } else {
+          return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        }
+      });
     }
   },
   partialsDir: ['views/partials/'],
