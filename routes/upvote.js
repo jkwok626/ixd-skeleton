818a0fc6dc287102d5d41f82a939aeab2a581bd0
Jorge Castaneda <castaneda.jorge7@gmail.com>
2021-03-13 18:20:02 -0800

    fix vote

--- routes/upvote.js
+++ routes/upvote.js
@@ -2,35 +2,75 @@ var data = require("../products.json")['products'];
 
 exports.upVote = function (request, response) {
     var buttonValue = request.query.id.split(',');
+    var productID = buttonValue[1] - 1;
 
-    // home page, else product page
-    if (buttonValue[0] == 0) {
-        var productID = buttonValue[1] - 1;
-        var newScore = data[productID].voteScore + 1;
-        data[productID].voteScore = newScore;
+    if (!data[productID].voteState) {
+        // home page, else product page
+        if (buttonValue[0] == 0) {
+            var newScore = data[productID].voteScore + 1;
+            data[productID].voteScore = newScore;
+            data[productID].voteState = true;
+            data[productID].upvoteState = true;
+            data[productID].downvoteState = false;
 
-        data.sort(function(a, b) {
-            return b.voteScore - a.voteScore;
-        });
+            data.sort(function (a, b) {
+                return b.voteScore - a.voteScore;
+            });
 
-        for(i = 0; i < data.length; i++) {
-            data[i].ranking = (i+1);
+            for (i = 0; i < data.length; i++) {
+                data[i].ranking = (i + 1);
+            }
+
+            response.redirect("/home");
+        } else {
+            var newScore = data[productID].voteScore + 1;
+            data[productID].voteScore = newScore;
+            data[productID].voteState = true;
+            data[productID].upvoteState = true;
+            data[productID].downvoteState = false;
+
+            data.sort(function (a, b) {
+                return b.voteScore - a.voteScore;
+            });
+
+            for (i = 0; i < data.length; i++) {
+                data[i].ranking = (i + 1);
+            }
+
+            response.redirect("/product/" + buttonValue[1]);
         }
-
-        response.redirect("/home");
     } else {
-        var productID = buttonValue[1] - 1;
-        var newScore = data[productID].voteScore + 1;
-        data[productID].voteScore = newScore;
+        // home page, else product page
+        if (buttonValue[0] == 0) {
+            var newScore = data[productID].voteScore + 2;
+            data[productID].voteScore = newScore;
+            data[productID].upvoteState = true; // disable
+            data[productID].downvoteState = false; // enable
 
-        data.sort(function(a, b) {
-            return b.voteScore - a.voteScore;
-        });
+            data.sort(function (a, b) {
+                return b.voteScore - a.voteScore;
+            });
 
-        for(i = 0; i < data.length; i++) {
-            data[i].ranking = (i+1);
+            for (i = 0; i < data.length; i++) {
+                data[i].ranking = (i + 1);
+            }
+
+            response.redirect("/home");
+        } else {
+            var newScore = data[productID].voteScore + 2;
+            data[productID].voteScore = newScore;
+            data[productID].upvoteState = true;
+            data[productID].downvoteState = false;
+
+            data.sort(function (a, b) {
+                return b.voteScore - a.voteScore;
+            });
+
+            for (i = 0; i < data.length; i++) {
+                data[i].ranking = (i + 1);
+            }
+
+            response.redirect("/product/" + buttonValue[1]);
         }
-
-        response.redirect("/product/" + buttonValue[1]);
     }
 };
